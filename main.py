ddf592477ba267389a1686391676ce2e989b98cf
David James Brown <browndj24@gmail.com>
2021-10-27 22:26:19 +0100

    update create_item

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    description: Optional[str]= None
+    description: Optional[str] = None
     price: float
     tax: Optional[float] = None
 
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price-with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/")
